883eaa52a2aae3d284d35a36f98437c41ed07c30
Mark079 <joaopedromaximo@gmail.com>
2024-03-24 13:51:02 -0300

    adds access to the API on the Form, adds isLoading and error functions to the button in addition to configuring the other field with the result of the request

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import { Button } from "../Button";
 import { Input } from "../Input";
 
-export const Form = ({ buttonTitle, text, setText, onCLick }) => {
-
+export const Form = ({ buttonTitle, text, setText, fetchAction, setOtherField }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const handleFetch = () => {
+        setIsLoading(true);
+        fetchAction()
+            .then(reponse => reponse.json())
+            .then(data => {
+                const chaves = Object.keys(data);
+                setOtherField(data[chaves[0]]);
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsLoading(false);
+                setError('Algo deu errado!');
+                setTimeout(() => setError(null), 3000)
+            });
+    };
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
             <Input text={text} setText={setText} />
-            <Button title={buttonTitle} onClick={onCLick} />
+            <Button title={error || buttonTitle} onClick={handleFetch} isLoading={isLoading} disabled={text.length < 1} />
         </div>
     );
 };
